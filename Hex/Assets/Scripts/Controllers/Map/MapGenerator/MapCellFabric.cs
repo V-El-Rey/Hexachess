64697464c027614fadda8b84fa8bf86091ae7cc4
ElRey <the.akopyan.valeriy@gmail.com>
2021-08-31 17:31:09 +0300

    Generation fix

--- Hex/Assets/Scripts/Controllers/Map/MapGenerator/MapCellFabric.cs
+++ Hex/Assets/Scripts/Controllers/Map/MapGenerator/MapCellFabric.cs
@@ -12,22 +12,46 @@ namespace Controllers.Map.MapGenerator
 
         private MapCell[,] _mapCellsArray;
 
-        public MapCellFabric(int width, int height)
+        public MapCellFabric(int width)
         {
             _width = width;
-            _height = height;
         }
 
         public MapCell[,] CreateMapCells(MapCellView prefab)
         {
-            _mapCellsArray = new MapCell[_width, _height];
-            for (int i = 0; i < _width; i++)
+            _mapCellsArray = new MapCell[_width, _width];
+            for (int i = 0; i < _width -1; i++)
             {
-                for (int j = 0; j < _height; j++)
+                for (int j = 0; j < _width; j++)
                 {
                     _mapCellsArray[i, j] = new MapCell {mapCellPrefab = prefab};
-                    Debug.Log($"{prefab.name}");
-                    //заполнить клеточку тута
+                    // Debug.Log($"{prefab.name}");
+                    // заполнить клеточку тута
+
+                    if ((j < 2 && (i < 1)) ^ (j > _width - 3) && (i < 1))
+                    {
+                        _mapCellsArray[i, j].waterBorder = true;
+                    }
+
+                    if ((j < 1 && i == 2) ^ (j == _width -1) && (i == 2))
+                    {
+                        _mapCellsArray[i, j].waterBorder = true;
+                    }
+
+                    if ((j < 1 && i == 1) ^ (j > _width - 3) && (i == 1))
+                    {
+                        _mapCellsArray[i, j].waterBorder = true;
+                    }
+
+                    if (i == 3 && j == _width - 1)
+                    {
+                        _mapCellsArray[i, j].waterBorder = true;
+                    }
+
+                    // if (i != _width / 2 - 1 && ((j < 1) ^ (j > _width - 2)))
+                    // {
+                    //     _mapCellsArray[i, j].waterBorder = true;
+                    // }
                 }
             }
             return _mapCellsArray;
--- Hex/Assets/Scripts/Struct/MapCellStruct/MapCell.cs
+++ Hex/Assets/Scripts/Struct/MapCellStruct/MapCell.cs
@@ -5,6 +5,7 @@ namespace Struct.MapCellStruct
     public class MapCell
     {
         public MapCellView mapCellPrefab;
+        public bool waterBorder;
         public bool isWalkable;
         public bool enemySpawnMarker;
         public bool chestSpawnMarker;
